4a3706730ddd7a19d4c43c12baea7c5b868e74a8
lf7817 <535536456@qq.com>
2021-12-14 23:01:19 +0800

    chore: backup

--- packages/taro-example/config/index.js
+++ packages/taro-example/config/index.js
@@ -22,6 +22,7 @@ const config = {
     }
   },
   framework: 'react',
+  plugins: ['taro-plugin-inject-template'],
   mini: {
     postcss: {
       pxtransform: {
--- packages/taro-plugin-inject-template/src/index.ts
+++ packages/taro-plugin-inject-template/src/index.ts
@@ -1,3 +1,17 @@
-export function taroPluginInjectTemplate(ctx: any, options: any) {
-  console.log(ctx, options);
+import { IPluginContext } from '@tarojs/service';
+import { TaroPluginInjectTemplateOptions } from './types';
+
+export default function taroPluginInjectTemplate(
+  ctx: IPluginContext,
+  options: TaroPluginInjectTemplateOptions
+) {
+  // const { printLog, processTypeEnum } = ctx.helper;
+
+  ctx.onBuildComplete(() => {
+    // processTypeEnum.MODIFY;
+    console.log(
+      '================================================================',
+      options
+    );
+  });
 }
